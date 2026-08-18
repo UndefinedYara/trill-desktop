f84fbf4f666a37c2c7210dd131f4eaaff771d943
Yara Khairat <yarakhairat10@gmail.com>
2026-08-18 13:28:34 +0300

    Migrated to @huggingface/transformers instead of the legacy @xenova/transformers

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  serverExternalPackages: ["onnxruntime-node"],
-  outputFileTracingExcludes: {
-    "/*": ["./node_modules/onnxruntime-node/**/*"],
-  },
-  output: "standalone",
 };
 
 export default nextConfig;
