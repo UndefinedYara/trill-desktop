da03ae34bd4b9fb7cd6438b3e45825f297ee282b
Yara Khairat <yarakhairat10@gmail.com>
2026-08-18 13:11:19 +0300

    Configured onnxruntime-node to run on WASM backend instead of the native node backend

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   // @xenova/transformers relies on this to be left out of the bundle 
   // and processed as an external dependency instead
   serverExternalPackages: ["onnxruntime-node"],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
