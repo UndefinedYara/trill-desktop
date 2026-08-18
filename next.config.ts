3ea78b6122e66b622586551a9f091926ab2e3cf3
Yara Khairat <yarakhairat10@gmail.com>
2026-08-18 13:00:41 +0300

    Fixed external onnxruntime-node dependency which @xenova/transformers relies on

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  // @xenova/transformers relies on this to be left out of the bundle 
+  // and processed as an external dependency instead
+  serverExternalPackages: ["onnxruntime-node"],
 };
 
 export default nextConfig;
